33e4378836239446b454ad46989f7b844e92404b
Paul Moros <pmoros@outlook.com>
2023-02-27 08:42:37 -0500

    refactor: add remote backend to global

--- global/storage_account/main.tf
+++ global/storage_account/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "=1.3.9"
+  backend "azurerm" {
+    storage_account_name = "stpaulmorosdefault001"
+    container_name       = "crterraformstate001"
+    key                  = "global/storage-account"
+    access_key           = "cri8Kac61+2I5usyQj2rOtf/baLjGLHtPXFxq9FOZc7DleKtcspPnJ74yl5UxiZI+7552kExcoT6+AStXYecSA=="
+  }
 }
 
 provider "azurerm" {
