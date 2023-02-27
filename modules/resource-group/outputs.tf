e52848c7beb4cbac0be9c227e41c97de0341e381
Paul Moros <pmoros@outlook.com>
2023-02-27 09:50:13 -0500

    fix: rg group output name

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,4 +1,4 @@
-output "output_name" {
-  value       = azurerm_resource_group.rg.id  
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name  
   description = "The ID of the Resource Group."  
 }
