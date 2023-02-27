196f90bc792cec787aed06b9bc04ea56215e7350
Paul Moros <pmoros@outlook.com>
2023-02-27 09:08:17 -0500

    refactor: move tfstates to another resource group

--- global/resource-group/main.tf
+++ global/resource-group/main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_version = "=1.3.9"
-  backend "azurerm" {
-    storage_account_name = "stpaulmorosdefault001"
-    container_name       = "crterraformstate001"
-    key                  = "global/resource-group"
-    access_key           = "cri8Kac61+2I5usyQj2rOtf/baLjGLHtPXFxq9FOZc7DleKtcspPnJ74yl5UxiZI+7552kExcoT6+AStXYecSA=="
-  }
+  # backend "azurerm" {
+  #   storage_account_name = "stpaulmorosdefault001"
+  #   container_name       = "crterraformstate001"
+  #   key                  = "global/resource-group"
+  #   access_key           = "cri8Kac61+2I5usyQj2rOtf/baLjGLHtPXFxq9FOZc7DleKtcspPnJ74yl5UxiZI+7552kExcoT6+AStXYecSA=="
+  # }
 }
 
-provider "azurerm" {    
-    features {}
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_resource_group" "rg-activity2-eastus" {
-  name     = "rg-activity2-${var.location}"
+resource "azurerm_resource_group" "rg-terraform-eastus" {
+  name     = "rg-terraform-${var.location}"
   location = var.location
 }
