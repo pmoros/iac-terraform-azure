1fa10815e0feecfff12c8383c18bc433283b87d8
Paul Moros <pmoros@outlook.com>
2023-03-03 21:00:29 -0500

    fix(global): missplaced ${}

--- global/storage-tfstates/main.tf
+++ global/storage-tfstates/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_storage_account" "tfstates" {
   name                     = "st${var.owner}tfstates${var.instance_number}"
   resource_group_name      = azurerm_resource_group.tfstates.name
   location                 = var.location
-  account_tier             = ${var.account_tier}
-  account_replication_type = ${var.account_replication_type}
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
   tags = var.tags
 }
@@ -25,5 +25,5 @@ resource "azurerm_storage_account" "tfstates" {
 resource "azurerm_storage_container" "tfstates" {
   name                  = "crtfstates${var.workload}"
   storage_account_name  = azurerm_storage_account.stpaulmorostfstate001.name
-  container_access_type = ${var.container_access_type}  
+  container_access_type = var.container_access_type
 }
