e59b8bb4811853907855ec5dc06a719fbddffaa9
Paul Moros <pmoros@outlook.com>
2023-03-04 18:51:02 -0500

    fix(subnet): add service delegation

--- modules/networks/subnet/main.tf
+++ modules/networks/subnet/main.tf
@@ -4,5 +4,12 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = each.value.prefixes
+  delegation {
+    name = "example-delegation"
 
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
